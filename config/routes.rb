f1fcbb82eca60495a9dd0b19c3b0ef78f7354ac7
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-20 20:53:02 -0500

    init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'render/index'
+
+
+
+ # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
+ root 'render#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
