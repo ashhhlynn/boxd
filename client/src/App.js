44cbfdce7f5a37131bdc7431456badb863c19a8b
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-30 00:57:24 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,23 @@ import Films from './components/Films'
 
 class App extends Component {
 
+  state={
+    films: []
+  }
 
   componentDidMount = () => {
     fetch("/films")
     .then((r) => r.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data))
+    this.setState({films: data})
+    ;
   }
 
   render() {
     return (
       <Router>
         <div className="App">   
-      
+      Hi {this.state.films[0].title}
             <Switch>
               <Route exact path="/" element={<Films />} />
 
