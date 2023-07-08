4152325ff083c54a0143666bad3d9aff4c76e0b8
ashhhlynn <ashleylynnwelch@gmail.com>
2023-07-08 18:47:43 -0500

    test

--- client/src/components/FilmsHome/Films.js
+++ client/src/components/FilmsHome/Films.js
@@ -3,8 +3,8 @@ import { Card, Popup, Icon, Image, Item } from 'semantic-ui-react'
 
 const Films = (props) => {
 	return (
-		<Card.Group itemsPerRow={5}>
-			{props.films.map((movie, index) => (
+        <Card.Group itemsPerRow={5}>
+            {props.films.map((movie, index) => (
                 <Popup flowing hoverable key={index} trigger={<Card><Image style={{height:"300px", width:"220px"}}src={movie.Poster} alt='movie'></Image></Card>}>
                     <div onClick={() => props.handleDiaryClick(movie)}>
                         <Popup.Content>
@@ -14,8 +14,8 @@ const Films = (props) => {
                         </Popup.Content>
                     </div>
                 </Popup>
-			))}
-		</Card.Group>
+            ))}
+        </Card.Group>
 	)
 }
 
--- render-build.sh
+++ render-build.sh
@@ -9,6 +9,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:reset
-bundle exec rails db:migrate
-bundle exec rails db:seed
+
 #if you have seeds to run add:
