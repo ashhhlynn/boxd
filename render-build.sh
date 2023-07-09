e3ab5ce94dabf31d392a81761493e5efc3dc9231
ashhhlynn <ashleylynnwelch@gmail.com>
2023-07-08 19:56:45 -0500

    test

--- render-build.sh
+++ render-build.sh
@@ -6,6 +6,7 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
