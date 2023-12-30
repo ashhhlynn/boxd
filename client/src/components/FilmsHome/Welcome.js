2e67234d8c05ff266cd44caa3d536f3502ad18b8
ashhhlynn <ashleylynnwelch@gmail.com>
2023-12-29 19:35:14 -0600

    deleted button

--- client/src/components/FilmsHome/Welcome.js
+++ client/src/components/FilmsHome/Welcome.js
@@ -1,15 +1,13 @@
-import { Button, Item } from 'semantic-ui-react'
+import { Item } from 'semantic-ui-react'
 import React from 'react'
-import { Link} from 'react-router-dom'
 
 const Welcome = () => {
 	return (
         <div className="bg">
             <Item style={{marginTop:"4%"}}><br></br><br></br><br></br><br></br><br></br><br></br>
-                <h1>The social network for film.</h1>
+                <h1 style={{marginTop:"3.5%"}}>The social network for film.</h1>
                 <h2>Track films you've watched.</h2>
                 <h2>Tell your friends what's good.</h2>
-                <Button circular as={Link} to ="/login" size="big">GET STARTED</Button>  
             </Item>
         </div>
 	)
