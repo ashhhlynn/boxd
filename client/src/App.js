f684c87dadcea8a12b4196b9a6972cbb5f1a9e7e
ashhhlynn <ashleylynnwelch@gmail.com>
2024-02-28 03:31:10 -0600

    trying to fix indents

--- client/src/App.js
+++ client/src/App.js
@@ -6,108 +6,108 @@ import Footer from './components/Decorative/Footer'
 import SigninRegister from './components/SignUpIn/SigninRegister'
 import UserDiaries from './components/UserDiary/UserDiaries'
 import Home from './components/FilmsHome/Home'
+import Navbar from './components/Decorative/Navbar'
+import Watchlist from './components/UserWatchlist/Watchlist'
+import UserShow from './components/UserDiary/UserShow'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
-import Navbar from './components/Decorative/Navbar'
 import { checkUser } from "./components/actions/rootActions"
 import { fetchAllDF } from "./components/actions/rootActions"
-import Watchlist from './components/UserWatchlist/Watchlist'
 import { fetchWatchlistFilms } from "./components/actions/rootActions"
 import { fetchFeed } from "./components/actions/rootActions"
-import UserShow from './components/UserDiary/UserShow'
 
 class App extends Component {
 
-	state = {
-		userShow: null
-	}
+    state = {
+        userShow: null
+    }
 
-	componentDidMount = () => {
-		this.getUserProfile()
-		fetch("/diary_films")
-		.then(resp => resp.json())
-		.then(data => {
-			this.props.fetchAllDF(data)
-		})
-	}
-	
-	getUserProfile = () => {
-		fetch("/profile")
-		.then(resp => resp.json())
-		.then(data => {
-			this.props.checkUser(data)
-			if (data !== null) {
-				this.getWatchlistFilms()
-				this.getFeed()
-			}
-		})
-	}
+    componentDidMount = () => {
+        this.getUserProfile()
+        fetch("/diary_films")
+        .then(resp => resp.json())
+        .then(data => {
+            this.props.fetchAllDF(data)
+        })
+    }
+    
+    getUserProfile = () => {
+        fetch("/profile")
+        .then(resp => resp.json())
+        .then(data => {
+            this.props.checkUser(data)
+            if (data !== null) {
+                this.getWatchlistFilms()
+                this.getFeed()
+            }
+        })
+    }
 
-	getWatchlistFilms = () => {
-		fetch("/watchlist_films")
+    getWatchlistFilms = () => {
+        fetch("/watchlist_films")
         .then(resp => resp.json())
         .then(data => {
             this.props.fetchWatchlistFilms(data)
         })
-	}
+    }
 
-	getFeed = () => {
+    getFeed = () => {
         fetch("/feed")
         .then(resp => resp.json())
         .then(data => {
             this.props.fetchFeed(data)
         })
     }
-	
-	changeUserShow = (id) => {
+    
+    changeUserShow = (id) => {
        this.setState({userShow: id})
     }
 
-	render() {
-		return (
-	        <Router>
-			    <div className="app">
-				    <Navbar changeUserShow={this.changeUserShow} getUserProfile={this.getUserProfile}/>
-				    <Container>
-					    <Switch>
-						    <Route exact path="/">
-							    <Home currentUser={this.props.currentUser}/>
-						    </Route>
-						    <Route exact path="/login">
-							    <SigninRegister getUserProfile={this.getUserProfile}/>
-							</Route>
-						    <Route exact path="/userdiary">
-							    <UserDiaries/>
-						    </Route>
-						    <Route exact path="/watchlist">
-							    <Watchlist/>
-						    </Route>
-							<Route exact path="/profile">
-							    <UserShow userShow={this.state.userShow}/>
-						    </Route>
-					    </Switch>
-				    </Container>
-				    <Footer/>
-			    </div>
-			</Router>
-		)
-	}
+    render() {
+        return (
+            <Router>
+                <div className="app">
+                    <Navbar changeUserShow={this.changeUserShow} getUserProfile={this.getUserProfile}/>
+                    <Container>
+                        <Switch>
+                            <Route exact path="/">
+                                <Home currentUser={this.props.currentUser}/>
+                            </Route>
+                            <Route exact path="/login">
+                                <SigninRegister getUserProfile={this.getUserProfile}/>
+                            </Route>
+                            <Route exact path="/userdiary">
+                                <UserDiaries/>
+                            </Route>
+                            <Route exact path="/watchlist">
+                                <Watchlist/>
+                            </Route>
+                            <Route exact path="/profile">
+                                <UserShow userShow={this.state.userShow}/>
+                            </Route>
+                        </Switch>
+                    </Container>
+                    <Footer/>
+                </div>
+            </Router>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
     return { 
         currentUser: state.currentUser,
-		allDF: state.allDF
+        allDF: state.allDF
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return { 
-		checkUser: (user) => { dispatch(checkUser(user)) },
-		fetchAllDF: (data) => { dispatch(fetchAllDF(data)) },
-		fetchWatchlistFilms: (data) => { dispatch(fetchWatchlistFilms(data)) },
-		fetchFeed: (data) => { dispatch(fetchFeed(data)) }
-	}
+        checkUser: (user) => { dispatch(checkUser(user)) },
+        fetchAllDF: (data) => { dispatch(fetchAllDF(data)) },
+        fetchWatchlistFilms: (data) => { dispatch(fetchWatchlistFilms(data)) },
+        fetchFeed: (data) => { dispatch(fetchFeed(data)) }
+    }
 } 
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
