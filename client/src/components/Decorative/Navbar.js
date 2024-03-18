43d5bc6f1e0ebc70b878ff66cdedb58585f265ba
ashhhlynn <ashleylynnwelch@gmail.com>
2024-03-17 19:40:44 -0500

    More trying to fix indents

--- client/src/components/Decorative/Navbar.js
+++ client/src/components/Decorative/Navbar.js
@@ -6,12 +6,11 @@ import { connect } from 'react-redux'
 import { logOut } from "../actions/rootActions"
 
 class Navbar extends Component {
-
     state = {
-    	modalOpen: false
-	}
-
-	handleLogout = () => {
+        modalOpen: false
+    }
+    
+    handleLogout = () => {
         fetch("/logout", {
             method: 'DELETE',
             headers: {
@@ -20,62 +19,61 @@ class Navbar extends Component {
         })
         this.props.logOut()
     }    
-
-	handleOpen = () => {
-		this.setState({ modalOpen: true })
-	}
-	
-	handleClose = () => {
-		this.setState({ modalOpen: false })
-	}
-
-	changeUserShow = (userShow) => {
-		this.props.changeUserShow(userShow)
-		this.handleClose()
-	}
-
-	render() {
+    
+    handleOpen = () => {
+        this.setState({ modalOpen: true })
+    }
+    
+    handleClose = () => {
+        this.setState({ modalOpen: false })
+    }
+    
+    changeUserShow = (userShow) => {
+        this.props.changeUserShow(userShow)
+        this.handleClose()
+    }
+    
+    render() {
         return (
-      		<Menu className="nav">			
-				<Menu.Menu icon='labeled' style={{marginLeft:"4%", marginTop:"1%"}} position="left"><br></br>
-					<h1><Link to='/'>Boxd.</Link></h1>
-				</Menu.Menu>
-				<Menu.Menu style={{marginRight:"3%"}} position='right'>
-					{this.props.currentUser.length === 0 ?
-					<>
-						<Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/login'><Icon size="big" name="user circle outline"/></Link></Menu.Item>
-					</>
-						:
-						<>
-						<Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/userdiary'><Icon name="user circle" size="large" style={{ marginTop:"2%"}}/></Link> Diary</Menu.Item>
-						<Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/watchlist'><Icon name="eye" size="large" style={{ marginTop:"2%"}}/></Link> Watch</Menu.Item>
-						<Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Icon name="address book outline" onClick={this.handleOpen} size="large" style={{color:"#c7c7ff ", cursor:"pointer"}}/>Friends</Menu.Item>
-						<Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/'><Icon size="large" onClick={this.handleLogout} name="power off"/></Link></Menu.Item>
-						</>
-					}
-					<Modal
-					open={this.state.modalOpen}
-					onClose={this.handleClose}
-					closeIcon>
-            			<Modal.Content>
-							<UserPage addFollowFilms={this.props.addFollowFilms} removeFollowFilms={this.props.removeFollowFilms} changeUserShow={this.changeUserShow} currentUser={this.props.currentUser}/>
-            			</Modal.Content>
-          			</Modal>
-				</Menu.Menu>
-			</Menu>
-  		)
-	}
+            <Menu className="nav">
+                <Menu.Menu icon='labeled' style={{marginLeft:"4%", marginTop:"1%"}} position="left"><br></br>
+                    <h1><Link to='/'>Boxd.</Link></h1>
+                </Menu.Menu>
+                <Menu.Menu style={{marginRight:"3%"}} position='right'>
+                    {this.props.currentUser.length === 0 ?
+                        <>
+                        <Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/login'><Icon size="big" name="user circle outline"/></Link></Menu.Item>
+                        </>
+                    :
+                        <>
+                        <Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/userdiary'><Icon name="user circle" size="large" style={{ marginTop:"2%"}}/></Link> Diary</Menu.Item>
+                        <Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/watchlist'><Icon name="eye" size="large" style={{ marginTop:"2%"}}/></Link> Watch</Menu.Item>
+                        <Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Icon name="address book outline" onClick={this.handleOpen} size="large" style={{color:"#c7c7ff ", cursor:"pointer"}}/>Friends</Menu.Item>
+                        <Menu.Item style={{color:"#c7c7ff ", letterSpacing:".25px"}}><Link to='/'><Icon size="large" onClick={this.handleLogout} name="power off"/></Link></Menu.Item>
+                        </>
+                    }
+                    <Modal
+                    open={this.state.modalOpen}
+                    onClose={this.handleClose}
+                    closeIcon>
+                        <Modal.Content>
+                            <UserPage addFollowFilms={this.props.addFollowFilms} removeFollowFilms={this.props.removeFollowFilms} changeUserShow={this.changeUserShow} currentUser={this.props.currentUser}/>
+                        </Modal.Content>
+                    </Modal>
+                </Menu.Menu>
+            </Menu>
+        )
+    }
 }
-
 const mapStateToProps = (state) => {
-    return { 
-    	currentUser: state.currentUser
+    return {
+        currentUser: state.currentUser
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return { 
-		logOut: () => { dispatch(logOut()) }
+        logOut: () => { dispatch(logOut()) }
     }
 } 
 
--- client/src/components/FilmsHome/FilmModal.js
+++ client/src/components/FilmsHome/FilmModal.js
@@ -40,24 +40,24 @@ class FilmModal extends Component {
         }
         else {
             fetch("/diary_films", {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					title: this.props.film.title,
-					user_id: this.props.currentUser.id,
-					watch_date: this.props.film.watch_date,
-					year: this.props.film.year, 
-					poster: this.props.film.poster,
-					rating: 0,
-				})
-			})
-			.then((response) => response.json())
-			.then(data => {
-				window.alert("Added to diary.")
-				this.props.addDiaryFilm(data)
-			})
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: this.props.film.title,
+                    user_id: this.props.currentUser.id,
+                    watch_date: this.props.film.watch_date,
+                    year: this.props.film.year, 
+                    poster: this.props.film.poster,
+                    rating: 0,
+                })
+            })
+            .then((response) => response.json())
+            .then(data => {
+                window.alert("Added to diary.")
+                this.props.addDiaryFilm(data)
+            })
         }
     }
 	
@@ -78,119 +78,118 @@ class FilmModal extends Component {
 					year: this.props.film.year, 
 					poster: this.props.film.poster, 
 				})
-			})
-			.then((response) => response.json())
-			.then(data => {
-				window.alert("Added to watchlist.")
-				this.props.addWatchlistFilm(data)
-			})
-		}
-	}
-
+            })
+            .then((response) => response.json())
+            .then(data => {
+                window.alert("Added to watchlist.")
+                this.props.addWatchlistFilm(data)
+            })
+        }
+    }
+    
     removeFilmFromWatchlist = () => {
-		let x = this.props.watchlistFilms.find(f => f.watch_date === this.props.film.watch_date)
-		fetch(`/watchlist_films/` + x.id, {
-    		method: 'DELETE',
-    		headers: {
-			'Content-Type': 'application/json',	
-    		},
-		})
+        let x = this.props.watchlistFilms.find(f => f.watch_date === this.props.film.watch_date)
+        fetch(`/watchlist_films/` + x.id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',	
+            },
+        })
         this.props.removeWatchlistFilm(x)
     }
-
     render() {
-		let film = this.props.film
-		let feedEntries = this.props.feed.filter(f => f.title === film.title)
-		return (
-			<div className="filmModal" onClick={this.handleOpen}>
-				<Image src={film.poster}/>
-				<Modal
-				open={this.state.modalOpen}
-				onClose={this.handleClose}
-				closeIcon
-				>
-					<Modal.Content style={{marginTop:"1.75%"}}>
-						<h3 style={{fontFamily:"Helvetica", letterSpacing:".5px", fontSize:"17px"}}>
-							<Image floated="left" src={film.poster} style={{width:"82px", height:"120px", marginTop:"-1%"}}/>
-							{film.title}
-							{this.props.watchlistFilms.find(f => f.watch_date === this.props.film.watch_date) ?
-								<>
-								<Button size="large" onClick={this.removeFilmFromWatchlist} inverted animated style={{marginTop:"-1%", background:"none", color:"white"}} circular floated='right'>
-									<Button.Content visible>
-										<Icon name="eye slash"/>
-									</Button.Content>
-									<Button.Content hidden style={{fontSize:"13px"}}>
-										Unwatch
-									</Button.Content>
-								</Button>
-								</>
-							:
-								<>
-								<Button size="large" onClick={this.addUserWatchlistFilm} animated inverted style={{marginTop:"-1%", background:"none", color:"white"}} circular floated='right'>
-									<Button.Content visible>
-										<Icon name="eye"/>
-									</Button.Content>
-									<Button.Content hidden style={{fontSize:"13px"}}>
-										Watch
-									</Button.Content>
-								</Button>
-								</>
-							}
-							<Button size="large" onClick={this.addFilmToDiary} animated inverted style={{ marginTop:"-1%", background:"none",color:"white" }} circular floated='right'>
-								<Button.Content visible>
-									<Icon name="calendar check"/>
-								</Button.Content>
-								<Button.Content hidden style={{fontSize:"13px"}}>
-									Diary
-								</Button.Content>
-							</Button>
-						</h3>
-						{film.year}<br></br><br></br>
-						<p style={{marginTop:"-2%"}}>Average {this.state.score}</p>
-						<div className="filmrating" style={{marginTop:"-2%"}}>
-							<Rating className="stars" disabled rating={5} maxRating={5}/> 					
-						</div>
-						<br></br><br></br>
-						<Card.Group style={{marginLeft:"-6%", marginTop:"-2%"}} itemsPerRow={7} >
-							{feedEntries.map((f => (
-								<Card style={{marginLeft:"5%", textAlign:"center", fontSize:"12px", background:"inherit", boxShadow:"none", width:"30px"}}>
-									<Popup
-    								trigger={<Icon style={{fontSize:"280%", marginLeft:"20%"}}name="user circle"/>}
-    								content={f.user.username}
-    								basic
-  									/>
-                        			<Rating 
-                        			disabled
-                        			size="mini" 
-                        			rating={f.rating}  
-                        			maxRating={5}  
-									style={{fontSize:"8px", marginTop:"41%"}}
-                        			/>
-								</Card>
-							)))} 	
-						</Card.Group>
-					</Modal.Content>
-				</Modal>
-			</div>
-		)
-	}
+        let film = this.props.film
+        let feedEntries = this.props.feed.filter(f => f.title === film.title)
+        return (
+            <div className="filmModal" onClick={this.handleOpen}>
+                <Image src={film.poster}/>
+                <Modal
+                open={this.state.modalOpen}
+                onClose={this.handleClose}
+                closeIcon
+                >
+                    <Modal.Content style={{marginTop:"1.75%"}}>
+                        <h3 style={{fontFamily:"Helvetica", letterSpacing:".5px", fontSize:"17px"}}>
+                            <Image floated="left" src={film.poster} style={{width:"82px", height:"120px", marginTop:"-1%"}}/>
+                            {film.title}
+                            {this.props.watchlistFilms.find(f => f.watch_date === this.props.film.watch_date) ?
+                                <>
+                                <Button size="large" onClick={this.removeFilmFromWatchlist} inverted animated style={{marginTop:"-1%", background:"none", color:"white"}} circular floated='right'>
+                                    <Button.Content visible>
+                                        <Icon name="eye slash"/>
+                                    </Button.Content>
+                                    <Button.Content hidden style={{fontSize:"13px"}}>
+                                        Unwatch
+                                    </Button.Content>
+                                </Button>
+                                </>
+                            :
+                                <>
+                                <Button size="large" onClick={this.addUserWatchlistFilm} animated inverted style={{marginTop:"-1%", background:"none", color:"white"}} circular floated='right'>
+                                    <Button.Content visible>
+                                        <Icon name="eye"/>
+                                    </Button.Content>
+                                    <Button.Content hidden style={{fontSize:"13px"}}>
+                                        Watch
+                                    </Button.Content>
+                                </Button>
+                                </>
+                            }
+                            <Button size="large" onClick={this.addFilmToDiary} animated inverted style={{ marginTop:"-1%", background:"none",color:"white" }} circular floated='right'>
+                                <Button.Content visible>
+                                    <Icon name="calendar check"/>
+                                </Button.Content>
+                                <Button.Content hidden style={{fontSize:"13px"}}>
+                                    Diary
+                                </Button.Content>
+                            </Button>
+                        </h3>
+                        {film.year}<br></br><br></br>
+                        <p style={{marginTop:"-2%"}}>Average {this.state.score}</p>
+                        <div className="filmrating" style={{marginTop:"-2%"}}>
+                            <Rating className="stars" disabled rating={5} maxRating={5}/> 					
+                        </div>
+                        <br></br><br></br>
+                        <Card.Group style={{marginLeft:"-6%", marginTop:"-2%"}} itemsPerRow={7} >
+                            {feedEntries.map((f => (
+                                <Card style={{marginLeft:"5%", textAlign:"center", fontSize:"12px", background:"inherit", boxShadow:"none", width:"30px"}}>
+                                    <Popup
+                                    trigger={<Icon style={{fontSize:"280%", marginLeft:"20%"}}name="user circle"/>}
+                                    content={f.user.username}
+                                    basic
+                                    />
+                                    <Rating 
+                                    disabled
+                                    size="mini" 
+                                    rating={f.rating}  
+                                    maxRating={5}  
+                                    style={{fontSize:"8px", marginTop:"41%"}}
+                                    />
+                                </Card>
+                            )))}
+                        </Card.Group>
+                    </Modal.Content>
+                </Modal>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		currentUser: state.currentUser,
-		allDiaryFilms: state.allDF,
-		watchlistFilms: state.watchlistFilms,
-		feed: state.feed
-	}
+    return {
+        currentUser: state.currentUser,
+        allDiaryFilms: state.allDF,
+        watchlistFilms: state.watchlistFilms,
+        feed: state.feed
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		addDiaryFilm: (film) =>  { dispatch(addDiaryFilm(film)) },
-		addWatchlistFilm: (film) =>  { dispatch(addWatchlistFilm(film)) },
-		removeWatchlistFilm: (film) => { dispatch(removeWatchlistFilm(film)) }
-	}
+    return {
+        addDiaryFilm: (film) =>  { dispatch(addDiaryFilm(film)) },
+        addWatchlistFilm: (film) =>  { dispatch(addWatchlistFilm(film)) },
+        removeWatchlistFilm: (film) => { dispatch(removeWatchlistFilm(film)) }
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FilmModal)
