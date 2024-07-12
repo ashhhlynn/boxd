52495ab9503574dd422d8e386b4816f4825ee580
ashhhlynn <ashleylynnwelch@gmail.com>
2024-07-12 00:17:39 -0500

    Minor syntax cleaning

--- client/src/components/SearchFilms/ModalFeed.js
+++ client/src/components/SearchFilms/ModalFeed.js
@@ -3,20 +3,44 @@ import { Card, Popup, Icon, Rating } from 'semantic-ui-react';
 
 const ModalFeed = ({ feedEntries }) => {
     return (
-        <Card.Group style={{marginLeft:"-6%", marginTop:"-2%"}} itemsPerRow={7}>
+        <Card.Group 
+            itemsPerRow={7}
+            style={{
+                marginLeft:"-6%", 
+                marginTop:"-2%"
+            }}
+        >
             {feedEntries.map((f => (
-                <Card style={{marginLeft:"5%", textAlign:"center", fontSize:"12px", background:"inherit", boxShadow:"none", width:"30px"}}>
+                <Card style={{
+                    marginLeft:"5%", 
+                    textAlign:"center", 
+                    fontSize:"12px", 
+                    background:"inherit", 
+                    boxShadow:"none", 
+                    width:"30px"
+                }}>
                     <Popup
-                        trigger={<Icon style={{fontSize:"280%", marginLeft:"20%"}}name="user circle" />}
                         content={f.user.username}
                         basic
+                        trigger={
+                            <Icon 
+                                name="user circle" 
+                                style={{
+                                    fontSize:"280%", 
+                                    marginLeft:"20%"
+                                }}
+                            />
+                        }
                     />
                     <Rating 
                         disabled
                         size="mini" 
                         rating={f.rating}  
                         maxRating={5}  
-                        style={{fontSize:"8px", marginTop:"41%"}}
+                        style={{
+                            fontSize:"8px", 
+                            marginTop:"41%"
+                        }}
                     />
                 </Card>
             )))}
